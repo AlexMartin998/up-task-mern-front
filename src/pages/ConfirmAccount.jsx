9c9af1df8c621162b43b3efcb75315d25f287a44
Adrian Alex <alextravc.342@gmail.com>
2022-07-15 16:23:40 -0500

    [ADD]: useForm added

--- src/pages/ConfirmAccount.jsx
+++ src/pages/ConfirmAccount.jsx
@@ -1,3 +1,10 @@
 export const ConfirmAccount = () => {
-  return <div>ConfirmAccount</div>;
+  return (
+    <>
+      <h1 className="text-sky-600 font-black text-6xl capitalize">
+        Confirma tu cuenta y Comienza a crear tus {''}
+        <span className="text-slate-700">proyectos</span>
+      </h1>
+    </>
+  );
 };
