a26f29b0e6c83b71e84525698502c6fe0497b842
Adrian Alex <alextravc.342@gmail.com>
2022-07-16 21:35:24 -0500

    [ADD]: NewPassword completed

--- src/pages/NewPassword.jsx
+++ src/pages/NewPassword.jsx
@@ -1,4 +1,58 @@
+import { useEffect, useState } from 'react';
+import { useParams, Link } from 'react-router-dom';
+
+import { useForm } from '../hook/useForm';
+import { fetchWithoutToken } from '../helper/fetch';
+import { Alert } from '../components/Alert';
+
 export const NewPassword = () => {
+  const { token } = useParams();
+  const [formValues, handleInputChange, reset] = useForm({ newPassword: '' });
+  const { newPassword } = formValues;
+
+  const [isValidToken, setIsValidToken] = useState(false);
+  const [updatedPassword, setUpdatedPassword] = useState(false);
+  const [alerta, setAlerta] = useState({});
+  const { msg } = alerta;
+
+  useEffect(() => {
+    (async () => {
+      try {
+        await fetchWithoutToken(`/user/password-recovery/${token}`);
+
+        setAlerta({ msg: 'Coloca tu Nuevo Password', error: false });
+        setIsValidToken(true);
+      } catch (error) {
+        setAlerta({ msg: error.response.data.msg, error: true });
+      }
+    })();
+  }, []);
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    if (newPassword.length < 6) {
+      return setAlerta({
+        msg: 'El Password debe ser mínimo de 6 caracteres',
+        error: true,
+      });
+    }
+
+    try {
+      const { data } = await fetchWithoutToken(
+        `/user/password-recovery/${token}`,
+        { password: newPassword },
+        'POST'
+      );
+
+      setAlerta({ msg: data.msg, error: false });
+      setUpdatedPassword(true);
+    } catch (error) {
+      console.log(error.response.data);
+    }
+
+    reset();
+  };
+
   return (
     <>
       <h1 className="text-sky-600 font-black text-6xl capitalize">
@@ -6,28 +60,43 @@ export const NewPassword = () => {
         <span className="text-slate-700">proyectos</span>
       </h1>
 
-      <form className="my-10 bg-white shadow rounded-lg p-10">
-        <div className="my-5">
-          <label
-            className="uppercase text-gray-600 block text-xl font-bold"
-            htmlFor="newPassword"
-          >
-            Nuevo Password
-          </label>
-          <input
-            id="newPassword"
-            type="password"
-            placeholder="Escribe tu Nuevo Password"
-            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
-          />
-        </div>
+      <div className="mt-20 mb-10 bg-white shadow rounded-lg p-10">
+        {msg && <Alert alerta={alerta} />}
 
-        <input
-          type="submit"
-          value="Guardar Nuevo Password"
-          className="bg-sky-700 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
-        />
-      </form>
+        {isValidToken && !updatedPassword && (
+          <form onSubmit={handleSubmit}>
+            <div className="my-5">
+              <label
+                className="uppercase text-gray-600 block text-xl font-bold"
+                htmlFor="newPassword"
+              >
+                Nuevo Password
+              </label>
+              <input
+                id="newPassword"
+                type="password"
+                placeholder="Escribe tu Nuevo Password"
+                className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+                name="newPassword"
+                value={newPassword}
+                onChange={handleInputChange}
+              />
+            </div>
+
+            <input
+              type="submit"
+              value="Guardar Nuevo Password"
+              className="bg-sky-700 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
+            />
+          </form>
+        )}
+
+        {updatedPassword && (
+          <Link className="block text-center my-5 text-gray-500" to="/">
+            Log In
+          </Link>
+        )}
+      </div>
     </>
   );
 };
