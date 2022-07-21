3afdd8b8ba16164d85de8a67537e1cdb6bbf9ee2
Adrian Alex <alextravc.342@gmail.com>
2022-07-20 20:31:07 -0500

    [ADD]: tag colaborator added

--- src/components/ProjectPreview.jsx
+++ src/components/ProjectPreview.jsx
@@ -1,15 +1,26 @@
 import { Link } from 'react-router-dom';
+import { useAuth } from '../hook/useAuth';
 
 export const ProjectPreview = ({ project }) => {
-  const { name, _id, client } = project;
+  const { auth } = useAuth();
+  const { name, _id, client, owner } = project;
 
   return (
-    <div className="border-b p-5 flex">
-      <p className="flex-1">
-        {name}
+    <div className="border-b p-5 flex justify-between">
+      <div className="flex items-center gap-3">
+        <p className="flex-1">
+          {name}
 
-        <span className="text-sm text-gray-500 uppercase"> {client}</span>
-      </p>
+          <span className="text-sm text-gray-500 uppercase"> {client}</span>
+        </p>
+
+        {auth.uid !== owner && (
+          <p className="p-1 text-xs rounded-lg text-white bg-green-500 font-bold uppercase">
+            Colaborador
+          </p>
+        )}
+      </div>
+
       <Link
         to={`${_id}`}
         className="text-gray-600 hover:text-gray-800 uppercase text-sm font-bold"
